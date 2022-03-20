55583306f2d8d9f80c040df58de28b51d352f320
xddongx <rlaehdgus21@hanmail.net>
2022-03-20 15:56:32 +0900

    start mission 4

---
+++ 4차(8주차)/Basic/community/src/main/java/site/xddongx/community/repository/ShopReviewRepository.java
@@ -0,0 +1,7 @@
+package site.xddongx.community.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import site.xddongx.community.entity.ShopReviewEntity;
+
+public interface ShopReviewRepository extends CrudRepository<ShopReviewEntity, Long> {
+}
